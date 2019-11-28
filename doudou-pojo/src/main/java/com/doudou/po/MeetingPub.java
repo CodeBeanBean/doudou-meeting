9a65f37c80622086a1776f985774d34ec0f7d39b
doudou_global <349060332@qq.com>
2019-11-28 21:16:00 +0800

    会议发单功能添加

---
+++ doudou-pojo/src/main/java/com/doudou/po/MeetingPub.java
@@ -0,0 +1,32 @@
+package com.doudou.po;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class MeetingPub implements Serializable {
+   /** 主键Id UUID */
+    private String id;
+    /** 会议编号（按规则生成）*/
+    private String pcode;
+        /** 会议召开日期*/
+    private String ptime;
+        /**课题类别 */
+    private String tname;
+        /**会议主题*/
+    private String ptitle;
+        /** 讲者区域*/
+    private String pzone;
+        /** 发单人*/
+    private String uid;
+        /** 描述**/
+    private String remark;
+    /** 发单时间*/
+    private Date createdate;
+        /**发单状态*/
+    private Short status;
+
+
+}
